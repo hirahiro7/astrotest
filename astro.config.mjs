a201ef70df16f172219e7c41cec4bdb318424f43
hirahiro <hirahiroworks@gmail.com>
2024-07-17 21:38:43 +0900

    ユニット5まで

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://store.world.co.jp/s/brand/hirokohayashi/topics/20240712/',
+  site: 'https://example.com',
 });
